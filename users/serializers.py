106dad4cb0cbebbc3057eb5958c2f2600341005e
victorglim <victor.guedes888@gmail.com>
2023-04-14 13:49:56 -0300

    refactor: clean up serializer default (standard) to update User create UserPatch with add Meta class

--- users/serializers.py
+++ users/serializers.py
@@ -39,6 +39,18 @@ class UserSerializer(serializers.Serializer):
         if validated_data["is_employee"] is True:
             return User.objects.create_superuser(**validated_data, is_superuser=True)
         return User.objects.create_user(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get("username", instance.username)
+        instance.email = validated_data.get("email", instance.email)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.password = make_password(validated_data.get("password", instance.password))
+        instance.birthdate = validated_data.get("birthdate", instance.birthdate)
+
+        instance.save()
+
+        return instance
 
 
 class UserSpecificSerializer(serializers.ModelSerializer):
@@ -65,52 +77,12 @@ class UserSpecificSerializer(serializers.ModelSerializer):
         raise AuthenticationFailed("Você não tem permissão para acessar este usuário")
 
 
-class UserProfile(serializers.ModelSerializer):
-    username = serializers.CharField(
-        validators=[UniqueValidator(queryset=User.objects.all(), message="username already taken.")],
-        required=False,
-    )
-    email = serializers.CharField(
-        max_length=127,
-        validators=[UniqueValidator(queryset=User.objects.all(), message="email already registered.")],
-        required=False,
-    )
-    password = serializers.CharField(write_only=True, required=False)
-    birthdate = serializers.DateField(required=False)
-    first_name = serializers.CharField(max_length=50, required=False)
-    last_name = serializers.CharField(max_length=50, required=False)
-
+class UserPatch(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "email","first_name", "last_name","is_employee", "password", "birthdate", "is_superuser",]
         read_only_fields = ("id", "is_employee", "is_superuser")
         write_only_fields = "password"
 
-    def validate(self, data):
-        request = self.context.get("request")
-        user_id = self.context.get("user_id")
 
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed("Usuário não autenticado")
-
-        if request.user.is_employee:
-            return data
-
-        if user_id == request.user.id:
-            return data
-        if not request.user.is_employee and user_id != request.user.id:
-            raise PermissionDenied("Você não tem permissão para acessar este usuário")
-
-        raise AuthenticationFailed("Você não tem permissão para acessar este usuário")
-
-    def update(self, instance, validated_data):
-        instance.username = validated_data.get("username", instance.username)
-        instance.email = validated_data.get("email", instance.email)
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.password = make_password(validated_data.get("password", instance.password))
-        instance.birthdate = validated_data.get("birthdate", instance.birthdate)
-
-        instance.save()
-
-        return instance
+   
